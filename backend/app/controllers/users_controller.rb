1c6f73d0596bcab1125d80f7af13fa432c004b0a
mnavoy4 <mnavoy4@gmail.com>
2020-07-28 11:53:06 -0600

    updated sign in page with create new user feature and some styling using bootstrap

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -3,4 +3,12 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users, except: [:updated_at, :created_at]
   end
+
+  def create
+    @user = User.create(
+      username: params[:username],
+      password: params[:password]
+    )
+    redirect_to "http://localhost:3001/create-review.html?username=#{params[:username]}"
+  end
 end
--- frontend/index.js
+++ frontend/index.js
@@ -2,3 +2,14 @@ const loginForm = document.querySelector("#login-form");
 const searchParams = new URLSearchParams(window.location.search);
 const username = searchParams.get("username");
 const password = searchParams.get("password");
+const newPassword = document.querySelector("#new-password");
+const confirmPassword = document.querySelector("#confirm-password");
+const createUserButton = document.querySelector("#new-user-form-submit");
+
+
+createUserButton.addEventListener("click", function(event){
+  if (newPassword.value != confirmPassword.value){
+    alert("Your passwords do not match!");
+    event.preventDefault();
+  }
+})
